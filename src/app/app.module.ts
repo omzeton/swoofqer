1ec38946510e43469cc1a0d40bc57fc070ff60aa
omzeton <adamr6561@gmail.com>
2023-07-01 19:43:07 +0200

    create views folder

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,18 @@ import { HttpClientModule } from "@angular/common/http";
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
 import { NavbarComponent } from "./navbar/navbar.component";
-import { SplashScreenComponent } from "./splash-screen/splash-screen.component";
+import { SplashScreenComponent } from "./views/splash-screen/splash-screen.component";
 import { ButtonComponent } from "./design-system/button/button.component";
-import { DashboardComponent } from './dashboard/dashboard.component';
+import { DashboardComponent } from "./views/dashboard/dashboard.component";
 
 @NgModule({
-  declarations: [AppComponent, NavbarComponent, SplashScreenComponent, ButtonComponent, DashboardComponent],
+  declarations: [
+    AppComponent,
+    NavbarComponent,
+    SplashScreenComponent,
+    ButtonComponent,
+    DashboardComponent,
+  ],
   imports: [BrowserModule, HttpClientModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
